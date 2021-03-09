a28a7a9cdc2e314e633099e7415f1d299ddf71bf
zgpeace <zgpeace@gmail.com>
2021-03-10 06:47:34 +0800

    update 70

--- 70.爬楼梯.java
+++ 70.爬楼梯.java
@@ -8,7 +8,16 @@
 class Solution {
     public int climbStairs(int n) {
         if (n == 0 || n == 1) return 1;
-        return climbStairs(n - 1) + climbStairs(n - 2);
+        int pre = 1;
+        int current = 1;
+        int next;
+        for(int i = 2; i <= n; i++) {
+            next = current + pre;
+            pre = current;
+            current = next;
+        }
+
+        return current;
     }
 }
 // @lc code=end
