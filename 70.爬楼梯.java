9414d20db8b885fc8608c3c60b1c5885cc6bf988
zgpeace <zgpeace@gmail.com>
2021-03-09 12:19:55 +0800

    70

---
+++ 70.爬楼梯.java
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=70 lang=java
+ *
+ * [70] 爬楼梯
+ */
+
+// @lc code=start
+class Solution {
+    public int climbStairs(int n) {
+        if (n == 0 || n == 1) return 1;
+        return climbStairs(n - 1) + climbStairs(n - 2);
+    }
+}
+// @lc code=end
+
