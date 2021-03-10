739a9621826da5c4cb066715bf4f8e993c7916a1
zgpeace <zgpeace@gmail.com>
2021-03-10 13:05:17 +0800

    1

---
+++ 1.两数之和.java
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=java
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                return new int[]{map.get(nums[i]), i};
+            }
+            map.put(target - nums[i], i);
+        }
+
+        return new int[]{};
+    }
+}
+// @lc code=end
+
