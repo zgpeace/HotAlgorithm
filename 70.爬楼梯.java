1d5e738b0982d61bce6d0144629b0dfdc5d1f61f
zgpeace <zgpeace@gmail.com>
2021-03-10 23:18:37 +0800

    70

--- 70.爬楼梯.java
+++ 70.爬楼梯.java
@@ -7,11 +7,12 @@
 // @lc code=start
 class Solution {
     public int climbStairs(int n) {
-        if (n == 0 || n == 1) return 1;
+        if (n == 1) return 1;
+        if (n == 2) return 2;
         int pre = 1;
-        int current = 1;
+        int current = 2;
         int next;
-        for(int i = 2; i <= n; i++) {
+        for(int i = 3; i <= n; i++) {
             next = current + pre;
             pre = current;
             current = next;
