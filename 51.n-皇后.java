7d756991bd275bc0e144999e9170adaa50df3909
zgpeace <zgpeace@gmail.com>
2021-03-21 23:28:19 +0800

    51

---
+++ 51.n-皇后.java
@@ -0,0 +1,69 @@
+/*
+ * @lc app=leetcode.cn id=51 lang=java
+ *
+ * [51] N 皇后
+ */
+
+// @lc code=start
+class Solution {
+    public List<List<String>> solveNQueens(int n) {
+        char[][] chess = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int k = 0; k < n; k++) {
+                chess[i][k] = '.';
+            }
+        }
+        List<List<String>> res = new ArrayList<>();
+        solve(res, chess, 0);
+        return res;
+    }
+
+    private void solve(List<List<String>> res, char[][] chess, int row) {
+        if (row == chess.length) {
+            res.add(construct(chess));
+            return;
+        }
+        for (int col = 0; col < chess.length; col++) {
+            if (valid(chess, row, col)) {
+                chess[row][col] = 'Q';
+                solve(res, chess, row + 1);
+                chess[row][col] = '.';
+            }
+        }
+    }
+
+    private boolean valid(char[][] chess, int row, int col) {
+        // check up
+        for (int i = 0; i < row; i++) {
+            if (chess[i][col] == 'Q') {
+                return false;
+            }
+        }
+
+        // check right up
+        for (int i = row - 1, j = col + 1; i >= 0 && j < chess.length ; i--, j++) {
+            if (chess[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        // check left up
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (chess[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private List<String> construct(char[][] chess) {
+        List<String> path = new ArrayList<>();
+        for (int i = 0; i < chess.length; i++) {
+            path.add(new String(chess[i]));
+        }
+        return path;
+    }
+}
+// @lc code=end
+
