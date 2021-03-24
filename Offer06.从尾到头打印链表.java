6257bbc67e6c778c2a393c832cf3096de061d94d
zgpeace <zgpeace@gmail.com>
2021-03-24 22:46:06 +0800

    Offer06

---
+++ Offer06.从尾到头打印链表.java
@@ -0,0 +1,45 @@
+
+/*
+https://leetcode-cn.com/problems/cong-wei-dao-tou-da-yin-lian-biao-lcof/
+
+剑指 Offer 06. 从尾到头打印链表
+输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。
+
+ 
+
+示例 1：
+
+输入：head = [1,3,2]
+输出：[2,3,1]
+ 
+
+限制：
+
+0 <= 链表长度 <= 10000
+
+*/
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int[] reversePrint(ListNode head) {
+        LinkedList<Integer> stack = new LinkedList<>();
+        while (head != null) {
+            stack.addLast(head.val);
+            head = head.next;
+        }
+        int[] result = new int[stack.size()];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = stack.removeLast();
+        }
+
+        return result;
+    }
+}
